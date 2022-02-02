9e6c008058cfdda87728e2aeeb3e38a07fe68059
Jerome Gratelle <jerome.gratelle@air-indemnite.com>
2022-02-02 15:49:46 +0100

    security correction

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,6 +143,6 @@ MESSAGE_TAGS = {
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jerome.gratelle@gmail.com'
-EMAIL_HOST_PASSWORD = 'vB4#BSp7L$'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
